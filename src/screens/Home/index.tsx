3e7237c0818af711e00853a5da218d8d3d0def25
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-06-10 02:03:12 -0300

    feat: stylization home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Container } from "./styles";
+import Header from "../../components/Header";
 
 function Home() {
   return (
-    <View>
+    <Container>
+      <Header />
       <Text>Home</Text>
-    </View>
+    </Container>
   );
 }
 
---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  background-color: #141a29;
+  flex: 1;
+  padding: 10px 0;
+`;
