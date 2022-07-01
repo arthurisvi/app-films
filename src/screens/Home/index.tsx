f0a254f4e7af06fd848f29a35983c6983311535e
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-07-01 01:13:48 -0300

    feat: styles button and search

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Container } from "./styles";
+import { Container, SearchContainer, Input, SearchButton } from "./styles";
 import Header from "../../components/Header";
+import { Feather } from "@expo/vector-icons"
 
 function Home() {
   return (
     <Container>
-      <Header />
-      <Text>Home</Text>
+      <Header title="React Teste"/>
+      <SearchContainer>
+        <Input
+          placeholder="Ex: Vingadores"
+          placeholderTextColor="#ddd"
+        />
+        <SearchButton>
+        <Feather name = "search" size={30} color="#FFF"/>
+        </SearchButton>
+      </SearchContainer>
     </Container>
   );
 }
